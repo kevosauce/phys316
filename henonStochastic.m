88878c3086ef728e7c13d85876ae45976d7a93b8
Kevin Ullmann <kev.ullmann@gmail.com>
2016-11-07 12:11:58 -0600

    add more images. write 2nd part of b

---
+++ henonStochastic.m
@@ -0,0 +1,4 @@
+x = [0.61];
+y = [0.175];
+iters = 50000;
+[x,y] = applymap(x,y,iters);
--- henonmapC.m
+++ henonmapC.m
@@ -1,15 +1,13 @@
 clf
 xfp = 0.5689367002134077;
 yfp = 0.161844484385035;
-%xfp = 0.56893;
-%yfp = 0.16184;
 n = 50;
-t = linspace(0,0.2,n)';
+t = linspace(0,0.02,n)';
 xs = t + xfp;
 ys = yfp - 0.785*t;
 hold on
-plot(xs,ys,'-r','linewidth',40);
-iters = 100;
-applymap(xs,ys,iters);
-%axis([xfp,xfp+0.2,yfp-0.758*0.2,yfp]);
+iters = 180;
+title(['n=' n ' iters=' iters]);
+[xs,ys] = applymap(xs,ys,iters);
+axis([xfp-0.02,xfp+0.02,yfp-0.02,yfp+0.02]);
 hold off
