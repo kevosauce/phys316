15baebb80a796f269afe3a4ea1adc24dc686d7ea
Kevin Ullmann <kev.ullmann@gmail.com>
2016-11-30 13:16:05 -0600

    fix part B by taking transposes

--- applymap.m
+++ applymap.m
@@ -5,8 +5,8 @@ for i=1:iters
    x(:,i+1) = x(:,i) * cos(tau) - (y(:,i) - x(:,i).^2) * sin(tau);
    y(:,i+1) = x(:,i) * sin(tau) + (y(:,i) - x(:,i).^2) * cos(tau);
    % filter out points that have wandered off
-   %x(x > 1 | x < -1 | y > 1 | y < -1) = 0;
-   %y(x > 1 | x < -1 | y > 1 | y < -1) = 0;
+   x(x > 1 | x < -1 | y > 1 | y < -1) = 0;
+   y(x > 1 | x < -1 | y > 1 | y < -1) = 0;
 end
    
 plot(x',y','.')
--- henonmapB.m
+++ henonmapB.m
@@ -4,8 +4,10 @@ iters = 5000;
 [x,y] = applymap(x,y,iters);
 axis([0.53,0.63,0.1,0.2]);
 eps = 0.005;
-%find the first occurrence of a new point close to the original point
-[per, index] = max(abs(x(2:end,:) - x(1,:)) < eps & abs(y(2:end,:) - y(1,:)) < eps);
+% find all the points in the map within eps of the starting point
+pts = (abs(x(:,2:end) - x(:,1)) < eps & abs(y(:,2:end) - y(:,1)) < eps)';
+% find the first occurrence of a new point close to the original point
+[_, index] = max(pts);
 % starting points 1, 2, 5, and 6 are our resonant island trajectories (by inspection)
-cycles = index([1,2,5,6])
-
+periods = index'([1,2,5,6])
+% we find that periods = 140, 71, 265, and 81
